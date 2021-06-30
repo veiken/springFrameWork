26904a5a693e3682f0c55630510aace3acad013c
walif <walif@qq.com>
2021-06-30 23:56:26 +0800

    提交本地文件到git

---
+++ learn-01-helloGithub/src/main/java/HelloGithub.java
@@ -0,0 +1,7 @@
+public class HelloGithub {
+    public static void main(String[] args) {
+        System.out.println("hello Github !!" +
+                "This is the first learning project " +
+                "I'll learn it at home and at company");
+    }
+}
