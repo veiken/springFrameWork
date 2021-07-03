d9d7bcf582392e2c8067f173601132516281d4e8
walif <walif@qq.com>
2021-07-03 22:39:55 +0800

    delete

--- learn-01-helloGithub/src/main/java/HelloGithub.java
+++
@@ -1,7 +0,0 @@
-public class HelloGithub {
-    public static void main(String[] args) {
-        System.out.println("hello Github !!" +
-                "This is the first learning project " +
-                "I'll learn it at home and at company");
-    }
-}
